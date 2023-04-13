263b9ae031f76243c586cc919099a219d98861cd
novel bafagih <novelbafagih@student.ub.ac.id>
2023-04-13 19:23:54 +0700

    [ADD] Navigation Animation - custom navigation (from left, from right, to left, to right) - themes and colors - plugin safearg in build gradle

--- app/src/main/java/com/example/storyapp/ui/fragment/LoginFragment.kt
+++ app/src/main/java/com/example/storyapp/ui/fragment/LoginFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.example.storyapp.R
 import com.example.storyapp.databinding.FragmentLoginBinding
 
@@ -17,6 +18,14 @@ class LoginFragment : Fragment() {
     ): View {
         binding = FragmentLoginBinding.inflate(inflater,container,false)
         // Inflate the layout for this fragment
+        setAction()
         return binding.root
+    }
+
+    private fun setAction() {
+        binding.register.setOnClickListener {
+            val destination = LoginFragmentDirections.actionLoginFragmentToRegisterFragment()
+            findNavController().navigate(destination)
+        }
     }
 }
--- app/src/main/java/com/example/storyapp/ui/fragment/RegisterFragment.kt
+++ app/src/main/java/com/example/storyapp/ui/fragment/RegisterFragment.kt
@@ -5,15 +5,26 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.example.storyapp.R
+import com.example.storyapp.databinding.FragmentRegisterBinding
 
 class RegisterFragment : Fragment() {
+    private lateinit var binding: FragmentRegisterBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_register, container, false)
+    ): View {
+        binding = FragmentRegisterBinding.inflate(inflater, container, false)
+        setAction()
+        return binding.root
+    }
+
+    private fun setAction(){
+        binding.loginButton.setOnClickListener {
+            val destination = RegisterFragmentDirections.actionRegisterFragmentToLoginFragment()
+            findNavController().navigate(destination)
+        }
     }
 
 }
